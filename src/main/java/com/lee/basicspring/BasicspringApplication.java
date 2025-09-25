de42df826bf6c9cdd2fdf55157bdcf8a7ac91b7a
luda412 <201810791@sangmyung.kr>
2025-09-25 09:14:28 +0900

    [Setting] - First commit & project setting

---
+++ src/main/java/com/lee/basicspring/BasicspringApplication.java
@@ -0,0 +1,13 @@
+package com.lee.basicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicspringApplication.class, args);
+	}
+
+}
