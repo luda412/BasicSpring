7232e8ec6bfc5996f603f6aceb16c6018611d412
luda412 <201810791@sangmyung.kr>
2025-10-20 21:35:15 +0900

    [feat] - Join post mapping

--- src/main/java/com/lee/basicspring/controller/CookieController.java
+++ src/main/java/com/lee/basicspring/controller/CookieController.java
@@ -1,20 +1,26 @@
 package com.lee.basicspring.controller;
 
-import com.lee.basicspring.data.dto.JoinRequest;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import com.lee.basicspring.data.dto.JoinRequest;
 import com.lee.basicspring.data.entity.Member;
 import com.lee.basicspring.service.MemberServiceimpl;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 
 
-@RestController
+
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/cookie-login")
 public class CookieController {
@@ -43,7 +49,7 @@ public class CookieController {
     }
 
     /* 
-     * 회원가입 페이지, 회원 가입 시 전달되는 data JoinRequest dto에 담아서 전달.
+     * 회원 가입 페이지 get 요청, 회원 가입 시 전달되는 data JoinRequest dto에 담아서 전달.
      */
     @GetMapping("/join")
     public String joinPage(Model model) {
@@ -54,6 +60,38 @@ public class CookieController {
         model.addAttribute("joinRequest",new JoinRequest());
 
         return "join";
+    }
+    
+    /* 
+     * 회원 가입 요청, dto에 담아서 post 요청
+     */
+    @PostMapping("/join")
+    public String join(@Valid @ModelAttribute JoinRequest joinRequest, BindingResult bindingResult, Model model) {
+        model.addAttribute("loginType", "cookie-login");
+        model.addAttribute("pageName", "쿠키 로그인");
+
+        //loingId duplication check | 여기서 get으로 꺼내려고할 때 값이 만약에 없으면 NPE 뜨니까 이 해결 방법 고안
+        if(memberServiceimpl.checkLoginIdDuplicate(joinRequest.getLoginId())){
+            bindingResult.addError(new FieldError("joinRequest", "loginId", "로그인 아이디가 중복됩니다."));
+        }
+
+
+        //nickName duplication check | 여기서 get으로 꺼내려고할 때 값이 만약에 없으면 NPE 뜨니까 이 해결 방법 고안
+        if(memberServiceimpl.checkNicknameDuplicate(joinRequest.getNickname())){
+            bindingResult.addError(new FieldError("joinRequest", "nickname", "닉네임이 중복됩니다."));
+        }
+
+        //password와 passwordCheck가 같은지 확인
+        if(!joinRequest.getPassword().equals(joinRequest.getPasswordCheck())){
+            bindingResult.addError(new FieldError("joinRequest", "passwordCheck", "비밀번호가 일치하지 않습니다."));
+        }
+
+        if(bindingResult.hasErrors()){
+            return "join";
+        }
+
+        memberServiceimpl.join(joinRequest);
+        return "redirect:/cookie-login";
     }
     
     
