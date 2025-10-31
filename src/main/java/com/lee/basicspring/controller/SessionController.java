ba1f0f298031d434ccb326cbc2690a4728c7e70d
luda412 <rudaroy@naver.com>
2025-10-31 15:18:50 +0900

    [Feat] Session login controller

---
+++ src/main/java/com/lee/basicspring/controller/SessionController.java
@@ -0,0 +1,35 @@
+package com.lee.basicspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import com.lee.basicspring.common.ControllerHelper;
+import com.lee.basicspring.data.entity.Member;
+import com.lee.basicspring.service.MemberServiceimpl;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/session-login")
+public class SessionController extends ControllerHelper {
+    private final MemberServiceimpl memberServiceimpl;
+
+    @GetMapping(value={"", "/"})
+    public String home(Model model, @SessionAttribute(name="memberId", required=false) Long memberId) {
+        setSessionAttributes(model);
+
+        Member loginMember = memberServiceimpl.getLoginMemberById(memberId);
+
+        if(loginMember != null){
+            model.addAttribute("nickname", loginMember.getNickname());
+        }
+        return "home";
+    }
+    
+
+}
