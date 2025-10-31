d1a92d9530cee6d21fdefd05a89d28f0b4a15bb6
luda412 <rudaroy@naver.com>
2025-10-31 19:18:36 +0900

    [Feat] Join page GetMapping

--- src/main/java/com/lee/basicspring/controller/SessionController.java
+++ src/main/java/com/lee/basicspring/controller/SessionController.java
@@ -1,15 +1,22 @@
 package com.lee.basicspring.controller;
 
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import com.lee.basicspring.data.dto.JoinRequest;
 import com.lee.basicspring.data.entity.Member;
 import com.lee.basicspring.service.MemberServiceimpl;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+
 
 
 @Controller
@@ -18,6 +25,10 @@ import lombok.RequiredArgsConstructor;
 public class SessionController {
     private final MemberServiceimpl memberServiceimpl;
 
+    /* 
+     * home page get request
+     * Get the memberId stored in the session
+     */
     @GetMapping(value={"", "/"})
     public String home(Model model, @SessionAttribute(name="memberId", required=false) Long memberId) {
 
@@ -28,6 +39,33 @@ public class SessionController {
         }
         return "home";
     }
+
+    /* 
+     * join page
+     * JoinRequestdto to receive input from the member
+     */
+    @GetMapping("/join")
+    public String joinPage(Model model) {
+        model.addAttribute("joinRequest", new JoinRequest());
+        return "join";
+    }
+    
+    /* 
+     * 
+     */
+    @PostMapping("/join")
+    public String join(@Valid @ModelAttribute JoinRequest joinRequest, BindingResult bindingResult, Model model) {
+        
+        //loginId 중복 체크
+        if()
+        
+        //닉네임 중복 체크
+
+
+        // password와 passwordCheck가 같은지 체크
+
+    }
+    
     
 
 }
