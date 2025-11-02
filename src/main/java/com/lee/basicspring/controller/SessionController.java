b5994ca5572b345d8f57d1a6d1f150925b586414
luda412 <rudaroy@naver.com>
2025-11-02 17:43:46 +0900

    [Feat] session Join post

--- src/main/java/com/lee/basicspring/controller/SessionController.java
+++ src/main/java/com/lee/basicspring/controller/SessionController.java
@@ -1,8 +1,9 @@
 package com.lee.basicspring.controller;
 
-import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,18 +52,39 @@ public class SessionController {
     }
     
     /* 
-     * 
+    * 회원 가입 요청, dto에 담아서 post 요청
+    * Check if the loginId already exists (duplicate)
+    * Check if the nickName already exists (duplicate)
+    * Check if password and passwordCheck are the same
+
+    * If there are any errors, return "join".
+    * Otherwise, redirect to "/sesion-login".
      */
     @PostMapping("/join")
     public String join(@Valid @ModelAttribute JoinRequest joinRequest, BindingResult bindingResult, Model model) {
         
         //loginId 중복 체크
-        if()
+        if(memberServiceimpl.checkLoginIdDuplicate(joinRequest.getLoginId())){
+            bindingResult.addError(new FieldError("joinRequest","loginId", "로그인 아이디가 중복됩니다."));
+        }
         
         //닉네임 중복 체크
-
+        if(memberServiceimpl.checkNicknameDuplicate(joinRequest.getNickname())){
+            bindingResult.addError(new FieldError("joinRequest","nickname", "닉네입이 중복됩니다."));
+        }
 
         // password와 passwordCheck가 같은지 체크
+        if(!joinRequest.getPassword().equals(joinRequest.getPasswordCheck())){
+            bindingResult.addError(new FieldError("joinRequest","passwordCheck", "비밀번호가 일치하지 않습니다."));
+        }
+        //Error 존재 시 Join page 다시 return
+        if(bindingResult.hasErrors()){
+            return "join";
+        }
+
+        //service를 통해 입력받은 member 정보 DB에 저장후 session-login(즉, home) 으로 redirect
+        memberServiceimpl.join(joinRequest);
+        return "redirect:/session-login";
 
     }
     
