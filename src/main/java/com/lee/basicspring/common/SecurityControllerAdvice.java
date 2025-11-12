0b4d927a95d232f24f6b2b262fe3c32ed1d1d572
luda412 <rudaroy@naver.com>
2025-11-12 14:22:28 +0900

    [Fix] renamed by SecurityController.java

--- src/main/java/com/lee/basicspring/common/SecurityControllerAdvice.java
+++ src/main/java/com/lee/basicspring/common/SecurityControllerAdvice.java
@@ -1,11 +1,11 @@
 package com.lee.basicspring.common;
 
-import com.lee.basicspring.controller.SecurityContorller;
+import com.lee.basicspring.controller.SecurityController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-@ControllerAdvice(assignableTypes = SecurityContorller.class)
+@ControllerAdvice(assignableTypes = SecurityController.class)
 public class SecurityControllerAdvice {
     @ModelAttribute
     public void setSecurityAttributes(Model model){
